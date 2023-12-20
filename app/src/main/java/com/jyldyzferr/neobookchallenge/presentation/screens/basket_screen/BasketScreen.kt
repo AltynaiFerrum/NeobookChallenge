d647d3d483fae8acfdb475e0a83d2770db72c4bd
Altynai Ferrum <pr.galaxy812@gmail.com>
2023-12-20 09:58:26 +0600

    Create a ui for Basket Screen, added drawables

--- app/src/main/java/com/jyldyzferr/neobookchallenge/presentation/screens/basket_screen/BasketScreen.kt
+++ app/src/main/java/com/jyldyzferr/neobookchallenge/presentation/screens/basket_screen/BasketScreen.kt
@@ -1,4 +1,66 @@
 package com.jyldyzferr.neobookchallenge.presentation.screens.basket_screen
 
-class BasketScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jyldyzferr.neobookchallenge.R
+import com.jyldyzferr.neobookchallenge.presentation.screens.main_screen.MainUiState
+import com.jyldyzferr.neobookchallenge.ui.theme.Purple80
+import com.jyldyzferr.neobookchallenge.ui.theme.SmallSpacing
+
+
+@Composable
+fun BasketScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(painter = painterResource(id = R.drawable.jjjjjj), contentDescription =)
+        Text(
+            text = "Eco-Market",
+            color = Color.White,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth(0.9f),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                contentColor = MaterialTheme.colorScheme.onBackground
+            )
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = SmallSpacing),
+                text = stringResource(id = R.string.instagram),
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
 }
