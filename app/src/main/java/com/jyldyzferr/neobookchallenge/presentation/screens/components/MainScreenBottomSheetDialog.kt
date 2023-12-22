fb1f9d0c009a399b4ec99c6608e2d6f12b9f1fe0
Altynai Ferrum <pr.galaxy812@gmail.com>
2023-12-22 09:52:29 +0600

    Create a ui for Other Bottom Dialog Sheet, added drawables, fixed bags

---
+++ app/src/main/java/com/jyldyzferr/neobookchallenge/presentation/screens/components/MainScreenBottomSheetDialog.kt
@@ -0,0 +1,98 @@
+package com.jyldyzferr.neobookchallenge.presentation.screens.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.jyldyzferr.neobookchallenge.R
+import com.jyldyzferr.neobookchallenge.ui.theme.MyGreen
+import com.jyldyzferr.neobookchallenge.ui.theme.SmallSpacing
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun MainScreenBottomSheetDialog() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val sheetState = rememberModalBottomSheetState()
+        var isSheetOpen by rememberSaveable {
+            mutableStateOf(false)
+        }
+        val scaffoldState = rememberBottomSheetScaffoldState()
+        val scope = rememberCoroutineScope()
+
+        BottomSheetScaffold(
+            scaffoldState = scaffoldState,
+            sheetContent = {
+           Text(
+                    text = stringResource(id = R.string.bottom),
+                    style = androidx.compose.material.MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = stringResource(id = R.string.bottom_1),
+                    style = androidx.compose.material.MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = stringResource(id = R.string.bottom_2),
+                    style = androidx.compose.material.MaterialTheme.typography.h6,
+                    textAlign = TextAlign.Center
+                )
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth(0.9f),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MyGreen,
+                        contentColor = MaterialTheme.colorScheme.onBackground
+                    )
+                ) {
+                    Text(
+                        modifier = Modifier.padding(vertical = SmallSpacing),
+                        text = stringResource(id = R.string.bottom_3),
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            },
+            sheetPeekHeight = 0.dp
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    scope.launch {
+                        scaffoldState.bottomSheetState.expand()
+                    }
+                }) {
+                    Text(text = "Open sheet")
+                }
+            }
+        }
+    }
+}
